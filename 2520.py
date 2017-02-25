8d2bb69c5586ed0ab86f796b48f8ed238d13feab
kaushikamaravadi <kaushik.amaravadi@gmail.com>
2017-02-25 00:25:27 -0600

    Delete 2520.py

--- 2520.py
+++
@@ -1,3 +0,0 @@
-for n in range(1000, 20000000000):
-    if n % 2 and n % 3 and n % 4 and n % 5 and n % 6 and n % 7 and n % 8 and n % 9 and n % 10 and n % 11 and n % 12 and n % 13 and n % 14 and n % 15 and n % 16 and n % 17 and n % 18 and n % 19 and n %20 == 0:
-        print(n)
