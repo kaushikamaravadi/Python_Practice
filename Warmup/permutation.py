71f0097693fb658317e5e4eac2bdada769544cd2
kaushikamaravadi <kaushik.amaravadi.com>
2017-02-26 23:15:39 -0600

    Added new files and changed Name

--- Warmup/permutation.py
+++ Warmup/permutation.py
@@ -1,10 +1,16 @@
 """Permutation"""
 def perms(s):
-    if(len(s)==1): return [s]
+    if(len(s)==1):
+        return [s]
     result=[]
+    #print(result)
     for i,v in enumerate(s):
+        print(i,v)
         result += [v+p for p in perms(s[:i]+s[i+1:])]
+        #print(result[2])
+        #print(result)
     return result
 
 
-print(perms('012'))
+print(perms('abc'))
+
