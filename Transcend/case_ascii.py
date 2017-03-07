b4c86fc4dd500503ff9c697056a7a853750e8904
kaushikamaravadi <kaushik.amaravadi.com>
2017-03-06 21:43:23 -0600

    Project Euler

---
+++ Transcend/case_ascii.py
@@ -0,0 +1,10 @@
+"""Change Lower Case to Upper Case""""
+
+output = input("enter anything")
+empty = ""
+for char in output:
+    if ord(char) >= 65 and ord(char)<= 90:
+        capital = ord(char) + 32
+        small = chr(capital)
+        empty = empty + small
+print(empty)
