5c8a3d2b8e12199cfd5f1f6bd48134a938ff2e39
kaushikamaravadi <kaushik.amaravadi@gmail.com>
2017-03-25 23:35:14 -0500

    Replaced

--- Transcend/movie.py
+++ Transcend/movie.py
@@ -1,39 +1,20 @@
-"""Vote. txt contains Five Columns would look something like 
-1       1       0.60    1.00    4/15/96 13:16:00
-1       2       0.60    1.00    10/22/96 16:37:31
-1       10      0.40    1.00    10/26/96 15:07:36
-1       13      0.00    0.20    4/15/96 12:13:36
-1       17      0.80    1.00    9/18/96 9:22:08
-
-Find total number of Unique Users
-Find total number of Unique Movies
-Find  10 Movies with Highest Rating
-Find 10 Movies with Lowest Rating
-Find 10 Users who watched most movies
-Find 10 Users who watched less movies
-"""
-
+"""Movie"""
 
 import csv
 from collections import Counter
 
 
-users = []
-movies = []
-ratings = []
-with open("/home/ubuntu/Downloads/Vote.txt", 'r') as path:
-    reader = csv.reader(path, dialect="excel-tab")
-    for content in reader:
-        users.append(content[0])
-        movies.append(content[1])
-        ratings.append(content[2])
-
-
 class Movie(object):
-    def __init__(self, user_id, movie_id, rating):
-        self.user_id = user_id
-        self.movie_id = movie_id
-        self.rating = rating
+    def __init__(self):
+        self.user_id = []
+        self.movie_id = []
+        self.rating = []
+        with open("/home/ubuntu/Downloads/Vote.txt", 'r') as path:
+            reader = csv.reader(path, dialect="excel-tab")
+            for content in reader:
+                self.user_id.append(content[0])
+                self.movie_id.append(content[1])
+                self.rating.append(content[2])
 
     def unique_users(self):
         return len(set(self.user_id))
@@ -43,7 +24,7 @@ class Movie(object):
 
     def highest_rating(self):
         data = zip(self.movie_id, self.rating)
-        list_of_movies= []
+        list_of_movies = []
         for k, v in data:
             if v == '1.00':
                 list_of_movies.append(k)
@@ -52,7 +33,7 @@ class Movie(object):
 
     def lowest_rating(self):
         delta = zip(self.movie_id, self.rating)
-        kiki= []
+        kiki = []
         for k, v in delta:
             if v == '0.00':
                 kiki.append(k)
@@ -69,11 +50,11 @@ class Movie(object):
         return users_with_lm
 
 
-m = Movie(users, movies, ratings)
+m = Movie()
 
 print(m.unique_users())
 print(m.unique_movies())
 print(m.highest_rating())
 print(m.lowest_rating())
 print(m.users_with_most_movies())
-#print(m.users_with_less_movies())
+# print(m.users_with_less_movies())
