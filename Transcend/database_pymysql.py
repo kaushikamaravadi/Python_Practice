11d148e8bce89770cb802881762a2d5c47b1d0bd
kaushikamaravadi <kaushik.amaravadi@gmail.com>
2017-03-28 22:28:07 -0500

    Private Work

---
+++ Transcend/database_pymysql.py
@@ -0,0 +1,10 @@
+import pymysql
+
+db = pymysql.connect(user='user', password='password', database='database_name')
+cursor = db.cursor()
+cursor.execute("Your Query")
+data = cursor.fetchall()
+for row in data:
+    print(row)
+print("Database version : %s " % data)
+db.close()
